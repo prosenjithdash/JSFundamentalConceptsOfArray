9b5c6270fba623a9bfe243ebfc8d5c64aaff8ec5
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-01-30 00:36:45 +0600

    Pop element added done

---
+++ ElementPush.js
@@ -0,0 +1,17 @@
+// Addremove Elements From Arrays Using Push,Pop Shift _ Unshift
+
+//PUSH
+const x = [45, 98, 32, 34];
+
+// Before Element push
+console.log('Before Element push',x)
+
+// After Element push
+x.push(50);
+x.push(60);
+x.push(80);
+x.push(20);
+x.push(40);
+x.push(30, 54);
+
+console.log('After Element push',x)
---
+++ Pop.js
@@ -0,0 +1,23 @@
+// Addremove Elements From Arrays Using Push,Pop Shift _ Unshift
+
+//POP
+const Y = [25, 48, 392, 994 , 343 , 32, 98];
+
+// Before Element POP
+console.log('Before Element POP', Y)
+
+// After Element 01 POP
+Y.pop()
+console.log('After Element 01 POP', Y)
+
+// After Element 02 POP
+Y.pop()
+Y.pop()
+console.log('After Element 02 POP', Y)
+
+// After Element 03 POP
+const out_03_1 = Y.pop()
+const out_03_2 = Y.pop()
+const out_03_3 = Y.pop()
+console.log('After Element 03 POP', Y)
+console.log('03 number POP elements: ', out_03_1, out_03_2, out_03_3)
